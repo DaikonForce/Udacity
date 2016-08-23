70df069d451d4950dc066592dc6be2cad60fdf3b
DaikonForce <DaikonForce@github.noreply.com>
2016-08-23 16:07:32 -0700

    completed bridge edges

--- cs215/3/bridge_edges.py
+++ cs215/3/bridge_edges.py
@@ -45,9 +45,9 @@ import collections
 
 
 def create_rooted_spanning_tree(G, root):   # may not work on more complex graphs
-    S = {node: {} for node in list(G.keys())}
+    S = {node: {} for node in G.keys()}
     open_list = collections.deque([root])
-    layer = {node: 0 for node in list(G.keys())}
+    layer = {node: 0 for node in G.keys()}
     layer[root] = 1
     visited = {root}
     while open_list:
@@ -75,7 +75,6 @@ def create_rooted_spanning_tree(G, root):   # may not work on more complex graph
                 else:
                     S[current][neighbor] = 'red'
                     S[neighbor][current] = 'red'
-    print()
     return S
 
 
@@ -97,21 +96,29 @@ def test_create_rooted_spanning_tree():
          'g': {'e': 1, 'f': 1}
          }
     S = create_rooted_spanning_tree(G, "a")
-    assert S == {'a': {'c': 'green', 'b': 'green'},
-                 'b': {'a': 'green', 'd': 'red'},
-                 'c': {'a': 'green', 'd': 'green'},
-                 'd': {'c': 'green', 'b': 'red', 'e': 'green'},
-                 'e': {'d': 'green', 'g': 'green', 'f': 'green'},
-                 'f': {'e': 'green', 'g': 'red'},
-                 'g': {'e': 'green', 'f': 'red'}
-                 } or {
-                 'a': {'b': 'green', 'c': 'green'},
-                 'b': {'d': 'green', 'a': 'green'},
-                 'c': {'d': 'red', 'a': 'green'},
-                 'e': {'f': 'green', 'd': 'green', 'g': 'green'},
-                 'd': {'b': 'green', 'c': 'red', 'e': 'green'},
-                 'f': {'g': 'red', 'e': 'green'}, 'g': {'f': 'red', 'e': 'green'}}
+    assert  S == ({'a': {'c': 'green', 'b': 'green'},    # doesn't always fork for some reason...
+                   'b': {'a': 'green', 'd': 'red'},
+                   'c': {'a': 'green', 'd': 'green'},
+                   'd': {'c': 'green', 'b': 'red', 'e': 'green'},
+                   'e': {'d': 'green', 'g': 'green', 'f': 'green'},
+                   'f': {'e': 'green', 'g': 'red'},
+                   'g': {'e': 'green', 'f': 'red'}} or
+            S == { 'a': {'b': 'green', 'c': 'green'},
+                   'b': {'d': 'green', 'a': 'green'},
+                   'c': {'a': 'green', 'd': 'red'},
+                   'd': {'b': 'green', 'c': 'red', 'e': 'green'},
+                   'e': {'d': 'green', 'f': 'green', 'g': 'green'},
+                   'f': {'e': 'green', 'g': 'red'},
+                   'g': {'e': 'green', 'f': 'red'}}), "\n S = {0}".format(S)    # this is not all possible permutations
 
+
+# S = {'a': {'b': 'green', 'c': 'green'},
+#      'b': {'d': 'green', 'a': 'green'},
+#      'c': {'d': 'red', 'a': 'green'},
+#      'd': {'b': 'green', 'c': 'red', 'e': 'green'},
+#      'e': {'g': 'green', 'd': 'green', 'f': 'green'},
+#      'f': {'g': 'red', 'e': 'green'},
+#      'g': {'f': 'red', 'e': 'green'}}
 ###########
 
 
@@ -120,7 +127,7 @@ def post_order(S, root):    # broken, probably keep idea of locking nodes until 
     po = {}
     po_counter = 1
     locked = {root}
-    layer = {node: 0 for node in list(S.keys())}
+    layer = {node: 0 for node in S.keys()}
     layer[root] = 1
     while open_list:
         current = open_list.pop()
@@ -160,9 +167,11 @@ def test_post_order():
          'g': {'e': 'green', 'f': 'red'}
          }
     po = post_order(S, 'a')
-    assert po == {'a': 7, 'b': 1, 'c': 6, 'd': 5, 'e': 4, 'f': 2, 'g': 3
-                  } or {
-                  'a': 7, 'b': 1, 'c': 6, 'd': 5, 'e': 4, 'f': 3, 'g': 2}
+    # because dictionaries won't run the same way twice when iterating through a dictionary, need to list all 4 answers.
+    assert (po == {'a': 7, 'b': 1, 'c': 6, 'd': 5, 'e': 4, 'f': 2, 'g': 3} or
+            po == {'a': 7, 'b': 1, 'c': 6, 'd': 5, 'e': 4, 'f': 3, 'g': 2} or
+            po == {'a': 7,  'b': 6, 'c': 5, 'd': 4, 'e': 3, 'f': 2, 'g': 1} or
+            po == {'a': 7,  'b': 6, 'c': 5, 'd': 4, 'e': 3, 'f': 1, 'g': 2}), "\n po = {0}".format(po)
 
 ##############
 
@@ -172,7 +181,6 @@ def number_of_descendants(S, root):
     # of that node, includes the node itself
     descendants = {node: 0 for node in list(S.keys())}
     open_list = collections.deque([root])
-    visited = set()
     paths = {root: root}
     while open_list:
         current = open_list.popleft()
@@ -196,7 +204,7 @@ def test_number_of_descendants():
          'g': {'e': 'green', 'f': 'red'}
          }
     nd = number_of_descendants(S, 'a')
-    assert nd == {'a': 7, 'b': 1, 'c': 5, 'd': 4, 'e': 3, 'f': 1, 'g': 1}
+    assert nd == {'a': 7, 'b': 1, 'c': 5, 'd': 4, 'e': 3, 'f': 1, 'g': 1}, "\n nd ={0}".format(nd)
 
 ###############
 
@@ -206,8 +214,25 @@ def lowest_post_order(S, root, po):
     # to the lowest post order value
     # below that node
     # (and you're allowed to follow 1 red edge)
-
-    pass
+    lowest_po_tuples = {node: [po[node], po[node]] for node in po}     # {node: (po, conditional po)}
+    open_list = [root]
+    paths = {root: root}
+    while open_list:
+        current = open_list.pop()
+        for neighbor in S[current]:
+            if neighbor not in paths:
+                if S[current][neighbor] == 'green':
+                    open_list.append(neighbor)
+                    paths[neighbor] = paths[current] + neighbor
+            if S[current][neighbor] == 'red':
+                if lowest_po_tuples[neighbor][0] < lowest_po_tuples[current][1]:
+                    lowest_po_tuples[current][1] = lowest_po_tuples[neighbor][0]
+        for path_node in paths[current]:
+            for i in range(2):
+                if lowest_po_tuples[current][i] < lowest_po_tuples[path_node][i]:
+                    lowest_po_tuples[path_node][i] = lowest_po_tuples[current][i]
+    lowest_po = {node: min(lowest_po_tuples[node]) for node in lowest_po_tuples}
+    return lowest_po
 
 
 def test_lowest_post_order():
@@ -221,8 +246,8 @@ def test_lowest_post_order():
          }
     po = post_order(S, 'a')
     l = lowest_post_order(S, 'a', po)
-    assert l == {'a': 1, 'b': 1, 'c': 1, 'd': 1, 'e': 2, 'f': 2, 'g': 2}
-
+    assert (l == {'a': 1, 'b': 1, 'c': 1, 'd': 1, 'e': 2, 'f': 2, 'g': 2} or                  # cases where 'b' == 1 -> 'f' or 'g' == 2
+            l == {'a': 1, 'b': 1, 'c': 1, 'd': 1, 'e': 1, 'f': 1, 'g': 1}), "l was invalid"   # cases where 'b' == 6 -> 'f' or 'g' == 1
 
 ################
 
@@ -232,6 +257,25 @@ def highest_post_order(S, root, po):
     # to the highest post order value
     # below that node
     # (and you're allowed to follow 1 red edge)
+    highest_po_tuples = {node: [po[node], po[node]] for node in po}     # {node: (po, conditional po)}
+    open_list = [root]
+    paths = {root: root}
+    while open_list:
+        current = open_list.pop()
+        for neighbor in S[current]:
+            if neighbor not in paths:
+                if S[current][neighbor] == 'green':
+                    open_list.append(neighbor)
+                    paths[neighbor] = paths[current] + neighbor
+            if S[current][neighbor] == 'red':
+                if highest_po_tuples[neighbor][0] > highest_po_tuples[current][1]:
+                    highest_po_tuples[current][1] = highest_po_tuples[neighbor][0]
+        for path_node in paths[current]:
+            for i in range(2):
+                if highest_po_tuples[current][i] > highest_po_tuples[path_node][i]:
+                    highest_po_tuples[path_node][i] = highest_po_tuples[current][i]
+    highest_po = {node: max(highest_po_tuples[node]) for node in highest_po_tuples}
+    return highest_po
     pass
 
 
@@ -245,8 +289,11 @@ def test_highest_post_order():
          'g': {'e': 'green', 'f': 'red'}
          }
     po = post_order(S, 'a')
+    print("\n{0}".format(po))
     h = highest_post_order(S, 'a', po)
-    assert h == {'a': 7, 'b': 5, 'c': 6, 'd': 5, 'e': 4, 'f': 3, 'g': 3}
+    print("\n{0}\n".format(h))
+    assert (h == {'a': 7, 'b': 5, 'c': 6, 'd': 5, 'e': 4, 'f': 3, 'g': 3} or  # cases where 'b' == 1
+            h == {'a': 7, 'b': 6, 'c': 6, 'd': 6, 'e': 3, 'f': 2, 'g': 2})  # cases where 'b' == 6
 
 #################
 
@@ -255,7 +302,20 @@ def bridge_edges(G, root):
     # use the four functions above
     # and then determine which edges in G are bridge edges
     # return them as a list of tuples ie: [(n1, n2), (n4, n5)]
-    pass
+    S = create_rooted_spanning_tree(G, root)
+    po = post_order(S, root)
+    lpo = lowest_post_order(S, root, po)
+    hpo = highest_post_order(S, root, po)
+    descendants = number_of_descendants(S, root)
+    bridges = []
+    for node1 in S.keys():
+        for node2 in S[node1].keys():
+            if po[node1] > po[node2]:   # check if is node1 is parent of node2
+                if (hpo[node2] <= po[node2] and
+                   lpo[node2] > po[node2] - descendants[node2] and
+                   not any('red' == S[node2][edge] for edge in S[node2])):  # if any edge is red, this node isn't counted
+                    bridges.append((node1, node2))
+    return bridges
 
 
 def test_bridge_edges():
@@ -271,9 +331,9 @@ def test_bridge_edges():
     assert bridges == [('d', 'e')]
 
 if __name__ == '__main__':
-    test_create_rooted_spanning_tree()
-    test_post_order()
-    test_number_of_descendants()
+    # test_create_rooted_spanning_tree()
+    # test_post_order()
+    # test_number_of_descendants()
     # test_lowest_post_order()
     # test_highest_post_order()
-    # test_bridge_edges()
+    test_bridge_edges()
