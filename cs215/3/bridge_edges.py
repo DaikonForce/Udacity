77593e409701af575cea7b7b5c6e38815819d909
DaikonForce <DaikonForce@github.noreply.com>
2016-08-24 00:50:53 -0700

    added comments and fixed up the testing

--- cs215/3/bridge_edges.py
+++ cs215/3/bridge_edges.py
@@ -47,10 +47,10 @@ import collections
 def create_rooted_spanning_tree(G, root):   # may not work on more complex graphs
     S = {node: {} for node in G.keys()}
     open_list = collections.deque([root])
-    layer = {node: 0 for node in G.keys()}
+    layer = {node: 0 for node in G.keys()}  # uses layers to check where the child node is relative to the parent node
     layer[root] = 1
     visited = {root}
-    while open_list:
+    while open_list:    # breadth first search
         current = open_list.popleft()
         S[current] = {}
         next_layer = layer[current] + 1
@@ -58,21 +58,20 @@ def create_rooted_spanning_tree(G, root):   # may not work on more complex graph
         prev_layer = layer[current] - 1
         prev_layer_count = 0
         for neighbor in G[current]:
-            # print(neighbor)
             if neighbor not in visited:
                 open_list.append(neighbor)
                 visited.add(neighbor)
-            if neighbor not in S[current].keys():
+            if neighbor not in S[current].keys():   # marks all non-visited edges as green
                 S[current][neighbor] = 'green'
                 S[neighbor][current] = 'green'
             if not layer[neighbor]:
                 layer[neighbor] = next_layer
-            else:
-                if layer[neighbor] == prev_layer and prev_layer_count < 1:
+            else:   # if layer_parent is not off by 1 from layer_child then mark as red
+                if layer[neighbor] == prev_layer and prev_layer_count < 1:  # counts number of parent connections
                     prev_layer_count += 1
-                elif layer[neighbor] == next_layer and next_layer_count < 2:
+                elif layer[neighbor] == next_layer and next_layer_count < 2:    # counts number of child connections
                     next_layer_count += 1
-                else:
+                else:   # if layers are off OR the child/parent connections have reached their limit, mark as red
                     S[current][neighbor] = 'red'
                     S[neighbor][current] = 'red'
     return S
@@ -96,20 +95,25 @@ def test_create_rooted_spanning_tree():
          'g': {'e': 1, 'f': 1}
          }
     S = create_rooted_spanning_tree(G, "a")
-    assert  S == ({'a': {'c': 'green', 'b': 'green'},    # doesn't always fork for some reason...
-                   'b': {'a': 'green', 'd': 'red'},
-                   'c': {'a': 'green', 'd': 'green'},
-                   'd': {'c': 'green', 'b': 'red', 'e': 'green'},
-                   'e': {'d': 'green', 'g': 'green', 'f': 'green'},
-                   'f': {'e': 'green', 'g': 'red'},
-                   'g': {'e': 'green', 'f': 'red'}} or
-            S == { 'a': {'b': 'green', 'c': 'green'},
-                   'b': {'d': 'green', 'a': 'green'},
-                   'c': {'a': 'green', 'd': 'red'},
-                   'd': {'b': 'green', 'c': 'red', 'e': 'green'},
-                   'e': {'d': 'green', 'f': 'green', 'g': 'green'},
-                   'f': {'e': 'green', 'g': 'red'},
-                   'g': {'e': 'green', 'f': 'red'}}), "\n S = {0}".format(S)    # this is not all possible permutations
+    S_b_d_red = {'a': {'b': 'green', 'c': 'green'},  # b-d red
+                 'b': {'a': 'green', 'd': 'red'},
+                 'c': {'a': 'green', 'd': 'green'},
+                 'd': {'b': 'red', 'c': 'green', 'e': 'green'},
+                 'e': {'d': 'green', 'f': 'green', 'g': 'green'},
+                 'f': {'e': 'green', 'g': 'red'},
+                 'g': {'e': 'green', 'f': 'red'}}
+    S_c_d_red = {'a': {'b': 'green', 'c': 'green'},  # c-d red
+                 'b': {'a': 'green', 'd': 'green', },
+                 'c': {'a': 'green', 'd': 'red'},
+                 'd': {'b': 'green', 'c': 'red', 'e': 'green'},
+                 'e': {'d': 'green', 'f': 'green', 'g': 'green'},
+                 'f': {'e': 'green', 'g': 'red'},
+                 'g': {'e': 'green', 'f': 'red'}}
+    # if checking whole dictionary, each sub-dictionary must match perfectly -> it's easier to check each sub-dictionary 1 at a time
+    b_d_true = all(S_b_d_red[node1] == S[node1] for node1 in G)
+    c_d_true = all(S_c_d_red[node1] == S[node1] for node1 in G)
+    if not(b_d_true or c_d_true):
+        assert 0, "\nSpanning tree is wrong.\nSpanning Tree(S) = {0}".format(S)
 
 
 # S = {'a': {'b': 'green', 'c': 'green'},
@@ -122,14 +126,17 @@ def test_create_rooted_spanning_tree():
 ###########
 
 
-def post_order(S, root):    # broken, probably keep idea of locking nodes until usable
+def post_order(S, root):
+    # numbers the nodes from bottom of the smallest leaves back to the root
+    # uses locked to lock and unlock the nodes we already looked at but isn't the smallest leaf
+    # that hasn't been added to the post order dictionary.
     open_list = [root]
     po = {}
     po_counter = 1
     locked = {root}
     layer = {node: 0 for node in S.keys()}
     layer[root] = 1
-    while open_list:
+    while open_list:    # depth first search
         current = open_list.pop()
         unused_green = 0
         for neighbor in S[current]:
@@ -143,7 +150,7 @@ def post_order(S, root):    # broken, probably keep idea of locking nodes until 
             po[current] = po_counter
             po_counter += 1
             for neighbor in S[current]:
-                if S[current][neighbor] == 'green' and neighbor not in po and neighbor != root:
+                if S[current][neighbor] == 'green' and neighbor not in po and neighbor != root:     # unlocks and adds to open_list
                     locked.discard(neighbor)
                     open_list.append(neighbor)
     po[root] = po_counter
@@ -171,7 +178,7 @@ def test_post_order():
     assert (po == {'a': 7, 'b': 1, 'c': 6, 'd': 5, 'e': 4, 'f': 2, 'g': 3} or
             po == {'a': 7, 'b': 1, 'c': 6, 'd': 5, 'e': 4, 'f': 3, 'g': 2} or
             po == {'a': 7,  'b': 6, 'c': 5, 'd': 4, 'e': 3, 'f': 2, 'g': 1} or
-            po == {'a': 7,  'b': 6, 'c': 5, 'd': 4, 'e': 3, 'f': 1, 'g': 2}), "\n po = {0}".format(po)
+            po == {'a': 7,  'b': 6, 'c': 5, 'd': 4, 'e': 3, 'f': 1, 'g': 2}), "\nPost Order(po) = {0}".format(po)
 
 ##############
 
@@ -181,10 +188,10 @@ def number_of_descendants(S, root):
     # of that node, includes the node itself
     descendants = {node: 0 for node in list(S.keys())}
     open_list = collections.deque([root])
-    paths = {root: root}
-    while open_list:
+    paths = {root: root}    # path leads from root to the node for each node
+    while open_list:    # depth first search
         current = open_list.popleft()
-        for node in paths[current]:
+        for node in paths[current]:     # counts up total number of descendants using path
             descendants[node] += 1
         for neighbor in S[current]:
             if neighbor not in paths:
@@ -204,7 +211,7 @@ def test_number_of_descendants():
          'g': {'e': 'green', 'f': 'red'}
          }
     nd = number_of_descendants(S, 'a')
-    assert nd == {'a': 7, 'b': 1, 'c': 5, 'd': 4, 'e': 3, 'f': 1, 'g': 1}, "\n nd ={0}".format(nd)
+    assert nd == {'a': 7, 'b': 1, 'c': 5, 'd': 4, 'e': 3, 'f': 1, 'g': 1}, "\nNumber of Descendants(nd) = {0}".format(nd)
 
 ###############
 
@@ -214,21 +221,21 @@ def lowest_post_order(S, root, po):
     # to the lowest post order value
     # below that node
     # (and you're allowed to follow 1 red edge)
-    lowest_po_tuples = {node: [po[node], po[node]] for node in po}     # {node: (po, conditional po)}
+    lowest_po_tuples = {node: [po[node], po[node]] for node in po}     # {node: (green-only post order, 1-red post order)}
     open_list = [root]
     paths = {root: root}
-    while open_list:
+    while open_list:    # depth first search
         current = open_list.pop()
         for neighbor in S[current]:
             if neighbor not in paths:
                 if S[current][neighbor] == 'green':
                     open_list.append(neighbor)
                     paths[neighbor] = paths[current] + neighbor
-            if S[current][neighbor] == 'red':
+            if S[current][neighbor] == 'red':   # if red, find lowest possible green connection
                 if lowest_po_tuples[neighbor][0] < lowest_po_tuples[current][1]:
                     lowest_po_tuples[current][1] = lowest_po_tuples[neighbor][0]
         for path_node in paths[current]:
-            for i in range(2):
+            for i in range(2):  # if green, find lowest possible red and green connection
                 if lowest_po_tuples[current][i] < lowest_po_tuples[path_node][i]:
                     lowest_po_tuples[path_node][i] = lowest_po_tuples[current][i]
     lowest_po = {node: min(lowest_po_tuples[node]) for node in lowest_po_tuples}
@@ -247,7 +254,7 @@ def test_lowest_post_order():
     po = post_order(S, 'a')
     l = lowest_post_order(S, 'a', po)
     assert (l == {'a': 1, 'b': 1, 'c': 1, 'd': 1, 'e': 2, 'f': 2, 'g': 2} or                  # cases where 'b' == 1 -> 'f' or 'g' == 2
-            l == {'a': 1, 'b': 1, 'c': 1, 'd': 1, 'e': 1, 'f': 1, 'g': 1}), "l was invalid"   # cases where 'b' == 6 -> 'f' or 'g' == 1
+            l == {'a': 1, 'b': 1, 'c': 1, 'd': 1, 'e': 1, 'f': 1, 'g': 1}), "\nLowest post order(l) = {0}".format(l)   # cases where 'b' == 6 -> 'f' or 'g' == 1
 
 ################
 
@@ -289,11 +296,9 @@ def test_highest_post_order():
          'g': {'e': 'green', 'f': 'red'}
          }
     po = post_order(S, 'a')
-    print("\n{0}".format(po))
     h = highest_post_order(S, 'a', po)
-    print("\n{0}\n".format(h))
     assert (h == {'a': 7, 'b': 5, 'c': 6, 'd': 5, 'e': 4, 'f': 3, 'g': 3} or  # cases where 'b' == 1
-            h == {'a': 7, 'b': 6, 'c': 6, 'd': 6, 'e': 3, 'f': 2, 'g': 2})  # cases where 'b' == 6
+            h == {'a': 7, 'b': 6, 'c': 6, 'd': 6, 'e': 3, 'f': 2, 'g': 2}), "\nHighest post order(h) = {0}".format(h)  # cases where 'b' == 6
 
 #################
 
@@ -328,12 +333,19 @@ def test_bridge_edges():
          'g': {'e': 1, 'f': 1}
          }
     bridges = bridge_edges(G, 'a')
-    assert bridges == [('d', 'e')]
+    assert bridges == [('d', 'e')], "\nBridge Edges(bridges) = {0}".format(bridges)
 
 if __name__ == '__main__':
-    # test_create_rooted_spanning_tree()
-    # test_post_order()
-    # test_number_of_descendants()
-    # test_lowest_post_order()
-    # test_highest_post_order()
+    print("Testing create rooted spanning tree")
+    test_create_rooted_spanning_tree()
+    print("Testing post order")
+    test_post_order()
+    print("Testing number of descendants")
+    test_number_of_descendants()
+    print("Testing lowest post order")
+    test_lowest_post_order()
+    print("Testing highest post order")
+    test_highest_post_order()
+    print("Testing bridge edges")
     test_bridge_edges()
+    print("Test complete")
